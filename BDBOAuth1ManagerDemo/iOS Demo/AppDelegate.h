2765a805eba6998108d14bb4e25d6f54efeb0733
Brad Bergeron <bradbergeron@gmail.com>
2013-10-11 14:22:18 -0400

    Adding demo app.

---
+++ BDBOAuth1ManagerDemo/iOS Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Bradley Bergeron on 11/10/2013.
+//  Copyright (c) 2013 Bradley David Bergeron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

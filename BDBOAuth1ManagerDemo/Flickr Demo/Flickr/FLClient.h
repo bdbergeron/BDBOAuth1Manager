bdef409af23ebd8395a3e86a3f63a9a3cbb3720e
Bradley David Bergeron <bradbergeron@gmail.com>
2014-05-14 22:05:23 -0400

    Migrate demo projects over to using framework modules (@import instead of #import)

--- BDBOAuth1ManagerDemo/Flickr Demo/Flickr/FLClient.h
+++ BDBOAuth1ManagerDemo/Flickr Demo/Flickr/FLClient.h
@@ -20,7 +20,7 @@
 //  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 //  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#import <Foundation/Foundation.h>
+@import Foundation;
 
 #import "BDBOAuth1RequestOperationManager.h"
 #import "BDBOAuth1SessionManager.h"

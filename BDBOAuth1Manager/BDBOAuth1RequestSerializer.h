0b6e36142f8ef79b656edbf095594071e95821e8
Bradley David Bergeron <bradbergeron@gmail.com>
2014-11-24 13:14:57 -0500

    Finalize documentation
    Fixes #25

--- BDBOAuth1Manager/BDBOAuth1RequestSerializer.h
+++ BDBOAuth1Manager/BDBOAuth1RequestSerializer.h
@@ -37,12 +37,29 @@ FOUNDATION_EXPORT NSString * const BDBOAuth1OAuthCallbackParameter;
 @interface BDBOAuth1Credential : NSObject
 <NSCoding, NSCopying>
 
+/**
+ *  Token ('oauth_token')
+ */
 @property (nonatomic, copy, readonly) NSString *token;
-@property (nonatomic, copy, readonly) NSString *secret;
-@property (nonatomic, copy)           NSString *verifier;
 
+/**
+ *  Token secret ('oauth_token_secret')
+ */
+@property (nonatomic, copy, readonly) NSString *secret;
+
+/**
+ *  Verifier ('oauth_verifier')
+ */
+@property (nonatomic, copy) NSString *verifier;
+
+/**
+ *  Check whether or not this token is expired.
+ */
 @property (nonatomic, assign, readonly, getter = isExpired) BOOL expired;
 
+/**
+ *  Additional custom (non-OAuth) parameters included with this credential.
+ */
 @property (nonatomic) NSDictionary *userInfo;
 
 
@@ -52,14 +69,49 @@ FOUNDATION_EXPORT NSString * const BDBOAuth1OAuthCallbackParameter;
  *  ---------------------------------------------------------------------------------------
  */
 #pragma mark Initialization
+
+/**
+ *  Create a new BDBOAuth1Credential instance with the given token, token secret, and verifier.
+ *
+ *  @param token      OAuth token ('oauth_token').
+ *  @param secret     OAuth token secret ('oauth_token_secret').
+ *  @param expiration Expiration date or nil if the credential does not expire.
+ *
+ *  @return New BDBOAuth1Credential.
+ */
 + (instancetype)credentialWithToken:(NSString *)token
                              secret:(NSString *)secret
                          expiration:(NSDate *)expiration;
+
+/**
+ *  Instantiate a new BDBOAuth1Credential instance with the given token, token secret, and verifier.
+ *
+ *  @param token      OAuth token ('oauth_token').
+ *  @param secret     OAuth token secret ('oauth_token_secret').
+ *  @param expiration Expiration date or nil if the credential does not expire.
+ *
+ *  @return New BDBOAuth1Credential.
+ */
 - (instancetype)initWithToken:(NSString *)token
                        secret:(NSString *)secret
                    expiration:(NSDate *)expiration;
 
+/**
+ *  Create a new BDBOAuth1Credential instance using parameters in the given URL query string.
+ *
+ *  @param queryString URL query string containing OAuth token parameters.
+ *
+ *  @return New BDBOAuth1Credential.
+ */
 + (instancetype)credentialWithQueryString:(NSString *)queryString;
+
+/**
+ *  Instantiate a new BDBOAuth1Credential instance using parameters in the given URL query string.
+ *
+ *  @param queryString URL query string containing OAuth token parameters.
+ *
+ *  @return New BDBOAuth1Credential.
+ */
 - (instancetype)initWithQueryString:(NSString *)queryString;
 
 @end
@@ -68,7 +120,14 @@ FOUNDATION_EXPORT NSString * const BDBOAuth1OAuthCallbackParameter;
 #pragma mark -
 @interface BDBOAuth1RequestSerializer : AFHTTPRequestSerializer
 
+/**
+ *  OAuth request token.
+ */
 @property (nonatomic, copy) BDBOAuth1Credential *requestToken;
+
+/**
+ *  OAuth access token.
+ */
 @property (nonatomic, copy, readonly) BDBOAuth1Credential *accessToken;
 
 
@@ -78,12 +137,56 @@ FOUNDATION_EXPORT NSString * const BDBOAuth1OAuthCallbackParameter;
  *  ---------------------------------------------------------------------------------------
  */
 #pragma mark Initialization
+
+/**
+ *  Create a new BDBOAuth1RequestSerializer instance for the given service with its consumerKey and consumerSecret.
+ *
+ *  @param service        Service (base URL) this request serializer is used for.
+ *  @param consumerKey    OAuth consumer key.
+ *  @param consumerSecret OAuth consumer secret.
+ *
+ *  @return New BDBOAuth1RequestSerializer for the specified service.
+ */
 + (instancetype)serializerForService:(NSString *)service
                      withConsumerKey:(NSString *)consumerKey
                       consumerSecret:(NSString *)consumerSecret;
+
+/**
+ *  Instantiate a new BDBOAuth1RequestSerializer instance for the given service with its consumerKey and consumerSecret.
+ *
+ *  @param service        Service (base URL) this request serializer is used for.
+ *  @param consumerKey    OAuth consumer key.
+ *  @param consumerSecret OAuth consumer secret.
+ *
+ *  @return New BDBOAuth1RequestSerializer for the specified service.
+ */
 - (instancetype)initWithService:(NSString *)service
                     consumerKey:(NSString *)consumerKey
                  consumerSecret:(NSString *)consumerSecret;
+
+
+/**
+ *  ---------------------------------------------------------------------------------------
+ * @name Storing the Access Token
+ *  ---------------------------------------------------------------------------------------
+ */
+#pragma mark Storing the Access Token
+
+/**
+ *  Save the given OAuth access token in the user's keychain for future use with this serializer's service.
+ *
+ *  @param accessToken OAuth access token.
+ *
+ *  @return Success of keychain item add/update operation.
+ */
+- (BOOL)saveAccessToken:(BDBOAuth1Credential *)accessToken;
+
+/**
+ *  Remove the access token currently stored in the keychain for this serializer's service.
+ *
+ *  @return Success of keychain item removal operation.
+ */
+- (BOOL)removeAccessToken;
 
 
 /**
@@ -92,17 +195,13 @@ FOUNDATION_EXPORT NSString * const BDBOAuth1OAuthCallbackParameter;
  *  ---------------------------------------------------------------------------------------
  */
 #pragma mark OAuth Parameters
-- (NSDictionary *)OAuthParameters;
-
 
 /**
- *  ---------------------------------------------------------------------------------------
- * @name Access Token
- *  ---------------------------------------------------------------------------------------
+ *  Retrieve the set of OAuth parameters to be included in authorized HTTP requests.
+ *
+ *  @return Dictionary of OAuth parameters.
  */
-#pragma mark Access Token
-- (BOOL)saveAccessToken:(BDBOAuth1Credential *)accessToken;
-- (BOOL)removeAccessToken;
+- (NSDictionary *)OAuthParameters;
 
 @end
 

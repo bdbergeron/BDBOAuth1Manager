f6d4d016b837104967629787ec4d691077be21da
Shawn Kim <shawn.kim@salesforce.com>
2015-06-09 17:43:23 -0500

    Add tests for RSA signatures

--- BDBOAuth1ManagerDemo/BDBOAuth1ManagerTests-OSX/BDBOAuth1ManagerTests_OSX.m
+++ BDBOAuth1ManagerDemo/BDBOAuth1ManagerTests-OSX/BDBOAuth1ManagerTests_OSX.m
@@ -8,6 +8,16 @@
 
 #import <Cocoa/Cocoa.h>
 #import <XCTest/XCTest.h>
+#import "BDBOAuth1RequestSerializer.h"
+
+@interface BDBOAuth1RequestSerializer ()
+
+- (NSString *)OAuthSignatureForMethod:(NSString *)method
+                            URLString:(NSString *)URLString
+                           parameters:(NSDictionary *)parameters
+                                error:(NSError *__autoreleasing *)error;
+
+@end
 
 @interface BDBOAuth1ManagerTests_OSX : XCTestCase
 
@@ -15,26 +25,40 @@
 
 @implementation BDBOAuth1ManagerTests_OSX
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+// Test cases from http://wiki.oauth.net/w/page/12238556/TestCases
+
+- (void) testOAuth1HMACSHA1Signatures {
+    NSString *expectedSignature = @"tR3+Ty81lMeYAr/Fid0kMTYa/WM=";
+
+    NSString *service = @"http://photos.example.net";
+    NSString *path = @"photos";
+
+    NSString *consumerKey = @"dpf43f3p2l4k3l03";
+    NSString *consumerSecret = @"kd94hf93k423kf44";
+
+    NSString *tokenKey = @"nnch734d00sl2jdk";
+    NSString *tokenSecret = @"pfkkdhi9sl3r4s00";
+
+    /*Expected base string:
+
+     GET&http%3A%2F%2Fphotos.example.net%2Fphotos&file%3Dvacation.jpg%26oauth_consumer_key%3Ddpf43f3p2l4k3l03%26oauth_nonce%3Dkllo9940pd9333jh%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1191242096%26oauth_token%3Dnnch734d00sl2jdk%26oauth_version%3D1.0%26size%3Doriginal
+     */
+    NSDictionary *parameters = @{ @"oauth_signature_method": @"HMAC-SHA1",
+                                  @"oauth_version": @"1.0",
+                                  @"oauth_consumer_key": consumerKey,
+                                  @"oauth_token": tokenKey,
+                                  @"oauth_timestamp": @"1191242096",
+                                  @"oauth_nonce": @"kllo9940pd9333jh",
+                                  @"file": @"vacation.jpg",
+                                  @"size": @"original" };
+
+    BDBOAuth1RequestSerializer *serializer = [BDBOAuth1RequestSerializer serializerForService:service withConsumerKey:consumerKey consumerSecret:consumerSecret];
+    [serializer setValue:[BDBOAuth1Credential credentialWithToken:tokenKey secret:tokenSecret expiration:[NSDate dateWithTimeIntervalSinceNow:3600]] forKey:NSStringFromSelector(@selector(accessToken))];
+
+    NSError *error;
+    NSString *signature = [serializer OAuthSignatureForMethod:@"GET" URLString:[NSString stringWithFormat:@"%@/%@", service, path] parameters:parameters error:&error];
+    XCTAssertEqualObjects(signature, expectedSignature, @"Computed OAuth signature does not match expected signature");
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
 
 @end

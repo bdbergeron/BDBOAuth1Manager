cae5f7f3c01dc782754b539b446ea61d5c095212
Keith Burgoyne <keith@silverorange.com>
2015-06-12 17:45:32 -0300

    ws

--- BDBOAuth1Manager/BDBOAuth1RequestOperationManager.h
+++ BDBOAuth1Manager/BDBOAuth1RequestOperationManager.h
@@ -118,10 +118,10 @@
                          failure:(void (^)(NSError *error))failure;
 
 - (void)fetchAccessTokenWithPathUsingXAuth:(NSString *)accessPath
-                          method:(NSString *)method
-                        username:(NSString *)username
-                        password:(NSString *)password
-                         success:(void (^)(BDBOAuth1Credential *accessToken))success
-                         failure:(void (^)(NSError *error))failure;
+                                    method:(NSString *)method
+                                  username:(NSString *)username
+                                  password:(NSString *)password
+                                   success:(void (^)(BDBOAuth1Credential *accessToken))success
+                                   failure:(void (^)(NSError *error))failure;
 
 @end
